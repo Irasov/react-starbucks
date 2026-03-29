d79fb1c74f17140225acba9722791338afb26f9d
Irasov <srnan@yandex.ru>
2026-03-29 12:43:52 +0500

    feat: add menu

--- react-starbucks/src/components/Header/index.tsx
+++ react-starbucks/src/components/Header/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import styles from "./Header.module.scss";
 
 const Header: React.FC = () => {
+  const [active, setActive] = React.useState(false);
+  const iconClick = () => {
+    setActive(!active);
+  }
   return (
     <div className={styles.root}>
       <div className="container">
@@ -9,15 +13,15 @@ const Header: React.FC = () => {
           <a href="#" className={styles.logo}>
             StarBucks
           </a>
-          <div className={styles.icon}>
-            <span></span>
-          </div>
-          <div className={styles.nav}>
+          <div className={`${styles.nav} ${active ? styles.active: ''}`} >
             <div className={styles.menu}>
               <a href="#" className={styles.item}>Home</a>
               <a href="#" className={styles.item}>Select</a>
               <a href="#" className={styles.item}>Shop</a>
             </div>
+          </div>
+          <div className={`${styles.icon} ${active ? styles.active : ''}`} onClick={iconClick}>
+            <span></span>
           </div>
         </div>
       </div>
