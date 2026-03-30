921598002b960a2d1e6c356345baac676df9e830
Irasov <srnan@yandex.ru>
2026-03-30 22:56:12 +0500

    feat: add decoration dor downBlock and Delicius component structure

--- react-starbucks/src/components/Delicius/index.tsx
+++ react-starbucks/src/components/Delicius/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import styles from "./Delicius.module.scss";
 
 const Delicius: React.FC = () => {
   return (
-    <></>
+    <div className={styles.root}>
+      <div className="container">
+        <div className={styles.body}>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
