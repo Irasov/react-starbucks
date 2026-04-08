aa927dad4ee67d21841d896d31fab60cbfde5c61
Irasov <srnan@yandex.ru>
2026-04-08 15:00:15 +0500

    feat: Search sstate and funtion change and clear for input

--- react-starbucks/src/components/Search/index.tsx
+++ react-starbucks/src/components/Search/index.tsx
@@ -2,10 +2,25 @@ import React from "react";
 import styles from "./Search.module.scss";
 
 const Search: React.FC = () => { 
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [value, setValue] = React.useState('');
+
+  const onClickClear = () => {
+    setValue('');
+    inputRef.current?.focus();
+  }
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  }
+
   return (
     <div className={styles.root}>
       <div className={styles.wrapper}>
-        <input type="text" className={styles.search} id="search" placeholder="Search Coffee..."/>
+        <input ref={inputRef} onChange={onChangeInput} value={value} type="text" className={styles.search} id="search" placeholder="Search Coffee..."/>
+        {value &&
+          <svg onClick={onClickClear} className={styles.clear} width="800px" height="800px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z"/></svg> 
+        }
       </div>
     </div>
   )
