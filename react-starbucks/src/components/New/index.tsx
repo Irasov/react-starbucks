c5bfc3283de0e40c451597a74b3b065d9f053182
Irasov <srnan@yandex.ru>
2026-04-22 17:03:05 +0500

    feat: add new component

---
+++ react-starbucks/src/components/New/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from './New.module.scss';
+
+const New: React.FC = () => {
+  return (
+    <section className={styles.root}>
+      <div className={styles.body}>
+        <div className={styles.info}>
+          <h2 className={styles.title}>
+              New our <span>Products</span> 
+          </h2>
+          <p className={styles.text}>
+            Have time to buy the most harmonious drinks in the new 
+            Starbucks coffee and don't forget about the discount! 
+            Starbucks coffee and don't forget about the discount!
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default New;
