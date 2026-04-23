53cc3096848229355f15de020c200223d4ba3ab6
Irasov <srnan@yandex.ru>
2026-04-23 12:44:16 +0500

    feat: add swiper

--- react-starbucks/src/components/New/index.tsx
+++ react-starbucks/src/components/New/index.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 import styles from './New.module.scss';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import axios from "axios";
+import CoffeCard from "../CoffeeBlock/CoffeCard";
+
+type CoffeItem = {
+  id: string,
+  imageUrl: string,
+  title: string,
+  sizes: string,
+  price: number,
+  category: number,
+  text: string,
+}
 
 const New: React.FC = () => {
+
+  const [coffee, setCoffee] = React.useState<CoffeItem[]>([]);
+
+  React.useEffect(()=>{
+    async function fetchCoffee() {
+      try {
+        const {data} = await axios.get(`https://69cca578ddc3cabb7bd15cb8.mockapi.io/items`);
+        setCoffee(data)
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchCoffee();
+  },[]);
+
+  const slides = coffee.map((item) => (
+    <SwiperSlide key={item.id}>
+      <CoffeCard {...item} />
+    </SwiperSlide>
+  ));
+
   return (
     <section className={styles.root}>
       <div className="container">
@@ -16,6 +52,29 @@ const New: React.FC = () => {
               Starbucks coffee and don't forget about the discount!
             </p>
           </div>
+            <Swiper
+              modules={[Pagination, Navigation]}
+              direction = 'horizontal'
+              slidesPerView = {3}
+              loop = {true}
+              // breakpoints={{
+              //   // when window width is >= 640px
+              //   640: {
+              //     width: 640,
+              //     slidesPerView: 2,
+              //   },
+              //   // when window width is >= 768px
+              //   768: {
+              //     width: 768,
+              //     slidesPerView: 3,
+              //   },
+              // }}
+            >
+              {slides}
+            </Swiper>
+            <div className="swiper-pagination"></div>
+            <div className="swiper-button-prev"></div>
+            <div className="swiper-button-next"></div>
         </div>
       </div>
     </section>
