65ec9c9c08891cfb78861c3b3a3fe226102875bb
Irasov <srnan@yandex.ru>
2026-04-23 09:38:24 +0500

    feat: add styles for components

--- react-starbucks/src/components/New/index.tsx
+++ react-starbucks/src/components/New/index.tsx
@@ -4,16 +4,18 @@ import styles from './New.module.scss';
 const New: React.FC = () => {
   return (
     <section className={styles.root}>
-      <div className={styles.body}>
-        <div className={styles.info}>
-          <h2 className={styles.title}>
-              New our <span>Products</span> 
-          </h2>
-          <p className={styles.text}>
-            Have time to buy the most harmonious drinks in the new 
-            Starbucks coffee and don't forget about the discount! 
-            Starbucks coffee and don't forget about the discount!
-          </p>
+      <div className="container">
+        <div className={styles.body}>
+          <div className={styles.info}>
+            <h2 className={styles.title}>
+                New our <span>Products</span> 
+            </h2>
+            <p className={styles.text}>
+              Have time to buy the most harmonious drinks in the new 
+              Starbucks coffee and don't forget about the discount! 
+              Starbucks coffee and don't forget about the discount!
+            </p>
+          </div>
         </div>
       </div>
     </section>
--- react-starbucks/src/pages/Home.tsx
+++ react-starbucks/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NewCafe from "../components/NewCafe";
 import Delicius from "../components/Delicius";
+import New from "../components/New";
 
 const Home: React.FC = () => {
   return (
     <main className="mainpage">
       <NewCafe />
       <Delicius />
+      <New />
     </main>
   )
 }
