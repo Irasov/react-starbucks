1acd71f2ea09bdd5fcbce6da922dd1df9006d3b0
Irasov <srnan@yandex.ru>
2026-04-29 14:58:19 +0500

    feat: add React.leazy

--- react-starbucks/src/App.tsx
+++ react-starbucks/src/App.tsx
@@ -1,20 +1,40 @@
+import React, {Suspense} from "react";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import NotFound from "./pages/NotFound";
-import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 import FullCoffee from "./pages/FullCoffee";
+import styles from './styles/App.module.scss'
+
+const Shop = React.lazy(() => import( "./pages/Shop"));
+
 function App() {
 
   return (
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route path="" element={<Home />} />
-          <Route path="shop" element={<Shop />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="coffee/:id" element={<FullCoffee />}/>
-          <Route path="*" element={<NotFound />} />
+          <Route path="shop" element={
+            <Suspense fallback={<div className={styles.loading}>Loading...</div>}>
+              <Shop />
+            </Suspense>
+            } />
+          <Route path="cart" element={
+            <Suspense fallback={<div className={styles.loading}>Loading...</div>}>
+              <Cart />
+            </Suspense>
+          } />
+          <Route path="coffee/:id" element={
+            <Suspense fallback={<div className={styles.loading}>Loading...</div>}>
+              <FullCoffee />
+            </Suspense>
+            }/>
+          <Route path="*" element={
+            <Suspense fallback={<div className={styles.loading}>Loading...</div>}>
+              <NotFound />
+            </Suspense>
+           } />
         </Route>
       </Routes>
       )
