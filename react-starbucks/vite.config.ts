4fd2adaf7d1517f2969b661a5f517273072d2a10
Irasov <srnan@yandex.ru>
2026-05-01 12:48:16 +0500

    feat: deploy

--- react-starbucks/vite.config.ts
+++ react-starbucks/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-starbucks",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
